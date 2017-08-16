68a4bf324dbe420d5199988dd2ea617e60d55d67
Jonathan Barrios <jeb2239@columbia.edu>
2017-08-15 22:19:50 -0400

    initial commit

---
+++ cpp/HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include <Speaker.h>
+#include <vector>
+using namespace std;
+using namespace Hello;
+
+int main(int argc, char *argv[]) {
+  Speaker* speaker = new Speaker();
+
+  speaker->sayHello();  
+}
